7694fa7ff135fd7a6a542a11d8807c1cef9a72bc
Rafaelkas <kosayev@bezeqint.net>
2021-06-20 10:13:33 +0300

    Add movies-app

---
+++ Day 18/movies-app/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieEditComponent } from './components/movie-edit/movie-edit.component';
+import { MoviesComponent } from './components/movies/movies.component';
+
+const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'movies', component: MoviesComponent},
+    {path: 'movies/:index', component: MovieDetailsComponent},
+    {path: 'movies/:index/edit', component: MovieEditComponent},
+    {path: 'account', component: AccountComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
