e49149743025ebd609b66b342ab2f623f51ec672
Rafaelkas <kosayev@bezeqint.net>
2021-07-13 09:52:08 +0300

    update hello-angular

--- Day 14/hello-angular/src/app/app.component.ts
+++ Day 14/hello-angular/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  // data
+  keyword: string = '';
+  results: string[] = [];
+  isBusy: boolean = false;
+
+  // methods
+  setKeyword(value: string) {
+    this.keyword = value;
+  }
+
+  search() {
+    console.log('searching ' + this.keyword);
+    this.isBusy = true;
+
+    setTimeout(() => {
+      this.isBusy = false;
+      this.results = [
+        this.keyword + '1',
+        this.keyword + '2',
+        this.keyword + '3'
+      ];
+    },2000);
+  }
 }
