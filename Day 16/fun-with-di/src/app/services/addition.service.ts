e23028d0851b01bac327b517bf7904e6d664919c
Rafaelkas <kosayev@bezeqint.net>
2021-07-23 13:41:53 +0300

    Add fun-with-di

---
+++ Day 16/fun-with-di/src/app/services/addition.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HistoryService } from "./history.service";
+
+@Injectable()
+export class AdditionService {
+    uid: number;
+    constructor(private historyService: HistoryService) {
+        console.log('creating addition service');
+        this.uid = Math.ceil(Math.random() * 1000000);
+    }
+
+    add(a: number, b: number): number {
+        const txt = `adding ${a} and ${b}`;
+        this.historyService.logRecord(txt);
+        return a+b;
+    }
+}
---
+++ Day 16/fun-with-di/src/app/services/history.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HistoryService {
+    private records: string[] = [];
+    constructor() {
+    }
+
+    logRecord(txt: string) {
+        this.records.push(txt);
+        console.log(this.records);
+    }
+
+    getHistory(): string[] {
+        return [...this.records];
+    }
+}
